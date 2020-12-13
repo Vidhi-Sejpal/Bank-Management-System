441ff6898839d7291bfcff8d352458d28119d5df
Vidhi Sejpal <57342621+Vidhi-Sejpal@users.noreply.github.com>
2020-12-13 11:06:47 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ from app import db, create_one_time_entry
 
 def create_tables():
     db.create_all()
-    create_one_time_entry()
+    
 
 if __name__ == "__main__":
     create_tables()
+    create_one_time_entry()
