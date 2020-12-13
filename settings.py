38e5945115f30ac3d1ec799bef74d406c67234df
Vidhi Sejpal <57342621+Vidhi-Sejpal@users.noreply.github.com>
2020-12-13 11:02:58 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
-from app import db
+from app import db, create_one_time_entry
 
 def create_tables():
     db.create_all()
+    create_one_time_entry()
 
 if __name__ == "__main__":
     create_tables()
